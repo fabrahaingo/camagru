19e1da5ef1dcdfb9594afddef36d77cf29f8c26f
Fabien RAHAINGOMANANA <fabrahaingo@hotmail.fr>
2018-08-21 14:49:00 +0200

    added cam feed

--- camera_section.php
+++ camera_section.php
@@ -1,8 +1,10 @@
 <!DOCTYPE html>
 
 <div id="camera_wrapper">
-  <video id="video" width="300" height="200"></video>
-  <button id="capture">Take photo</button>
-  <canvas id="canvas" width="300" height="200"></canvas>
-  <img id="photo" src="http://placekitten.com/g/300/200" alt="Photo of you">
+  <video id="video" width="200" height="150"></video>
+  <a href="#" id="capture">Take photo</a>
+  <canvas style="display: none;" id="canvas" width="200" height="150"></canvas>
+  <img id="photo" src="http://placekitten.com/g/200/150" alt="Photo of you">
 </div>
+
+<script src="js/take_photo.js"></script>
